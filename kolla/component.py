d28b3f472126e5fd47c514d73538335b42c90ca4
Berend Klein Haneveld <berendkleinhaneveld@gmail.com>
2022-10-27 20:15:37 +0200

    Basic working poc

--- kolla/component.py
+++ kolla/component.py
@@ -1,131 +1,58 @@
-from abc import abstractmethod
-from collections import defaultdict
+class Scheduler:
+    def __init__(self):
+        self.queue = []
 
-from observ import reactive, readonly
+    def add(self, component):
+        if component not in self.queue:
+            self.queue.append(component)
+
+    def flush(self):
+        while self.queue:
+            component = self.queue.pop(0)
+            component.flush_update()
+
+
+def schedule_update(component):
+    # SVELTE INTERNALS
+    scheduler.add(component)
+
+
+scheduler = Scheduler()
 
 
 class Component:
-    """Abstract base class for components"""
+    # SVELTE INTERNALS
+    def __init__(self, options, instance, create_fragment, props=None, **kwargs):
+        super().__init__()
 
-    __lookup_cache__ = defaultdict(dict)
+        from kolla.renderers import DictRenderer
 
-    def __init__(self, props=None):
-        self._props = readonly({} if props is None else props)
-        self._state = reactive({})
-        self._element = None
-        self._slots = {}
-        self._event_handlers = defaultdict(set)
-        self._lookup_cache = Component.__lookup_cache__[type(self)]
+        if props is None:
+            props = {}
 
-    @property
-    def props(self):
-        """The incoming props of this component."""
-        return self._props
+        if options is None:
+            options = {}
 
-    @props.setter
-    def props(self, value):
-        """Prevent overwriting the props attribute."""
-        raise RuntimeError("Not allowed to override props attribute")
+        self.renderer = options.get("renderer", DictRenderer())
 
-    @property
-    def state(self):
-        """The local state of this component."""
-        return self._state
+        def invalidate(variable, new_value):
+            self.ctx[variable] = new_value
+            self.dirty.add(variable)
+            schedule_update(self)
 
-    @state.setter
-    def state(self, value):
-        """Prevent overwriting the state attribute."""
-        raise RuntimeError("Not allowed to override state attribute")
+        self.ctx = instance(self, props, invalidate)
+        self.fragment = create_fragment(self.ctx, self.renderer)
+        self.dirty = set()
+        self.invalidate = invalidate
 
-    @property
-    def element(self):
-        """The root DOM element of this component."""
-        return self._element
+        def flush_update():
+            # Update the fragment
+            self.fragment.update(self.ctx, self.dirty)
+            # Clear the dirty flags
+            self.dirty.clear()
 
-    @element.setter
-    def element(self, value):
-        """Setter that is used by the internals of Kolla. Please don't use this."""
-        self._element = value
-
-    def mounted(self):
-        """Called after the component has been mounted.
-
-        A component is considered mounted after:
-
-        * All of its child components have been mounted.
-        * Its own DOM tree has been created and inserted into the parent container.
-        """
-        pass
-
-    def updated(self):
-        """Called after the component has updated its DOM tree.
-
-        A parent component's updated method is called after that of its child
-        components.
-        """
-        pass
-
-    def before_unmount(self):
-        """Called right before a component instance is to be unmounted.
-
-        Use this method to clean up manually created side effects such as timers, DOM
-        event listeners or server connections.
-
-        Note that there are no specific guarantees about the order of when this method
-        is called. A parent component's method might be called before that of its child
-        components.
-        """
-        pass
-
-    @abstractmethod
-    def render(self, renderer):  # pragma: no cover
-        pass
-
-    def emit(self, event, *args, **kwargs):
-        """Call event handlers for the given event. Any args and kwargs will be passed
-        on to the registered handlers."""
-        for handler in self._event_handlers[event].copy():
-            handler(*args, **kwargs)
-
-    def add_event_handler(self, event, handler):
-        """Adds an event handler for the given event."""
-        self._event_handlers[event].add(handler)
-
-    def remove_event_handler(self, event, handler):
-        """Removes an event handler for the given event."""
-        self._event_handlers[event].remove(handler)
-
-    def _lookup(self, name, context):
-        """
-        Helper method that is used in the template.
-        The method looks up variables that are mentioned in the template.
-        This provides some syntactic sugar so that users can leave out `self`,
-        `self.state` and `self.props`.
-        """
-        cache = self._lookup_cache
-        if method := cache.get(name):
-            return method(self, name, context)
-
-        def props_lookup(self, name, context):
-            return self.props[name]
-
-        def state_lookup(self, name, context):
-            return self.state[name]
-
-        def self_lookup(self, name, context):
-            return getattr(self, name)
-
-        def global_lookup(self, name, context):
-            return context[name]
-
-        if name in self.props:
-            cache[name] = props_lookup
-        elif name in self.state:
-            cache[name] = state_lookup
-        elif hasattr(self, name):
-            cache[name] = self_lookup
-        elif name in context:
-            cache[name] = global_lookup
-        else:
-            raise NameError(f"name '{name}' is not defined")
-        return self._lookup(name, context)
+        self.flush_update = flush_update
+        # Create the fragment
+        self.fragment.create()
+        # Mount the fragment onto the DOM
+        self.fragment.mount(options["target"], None)
