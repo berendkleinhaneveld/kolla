3c7b15c3405c9807e4ce6fe81e15f6899794e1f9
Berend Klein Haneveld <berendkleinhaneveld@gmail.com>
2022-11-21 20:28:45 +0100

    Add method for generating formatted code from templates

--- kolla/sfc/__init__.py
+++ kolla/sfc/__init__.py
@@ -11,21 +11,44 @@ def load(file):
     return load_from_string(source, component_name, file)
 
 
+def generate(file) -> str:
+    """Generate Python code from a .kolla file"""
+    file = Path(file)
+    component_name = "".join([part.capitalize() for part in file.stem.split("_")])
+    source = file.read_text(encoding="utf-8")
+    code = _generate_code(source, component_name)
+    result = ast.unparse(code)
+
+    try:
+        import black
+
+        result = black.format_file_contents(result, fast=False, mode=black.mode.Mode())
+    except ImportError:
+        pass
+
+    return result
+
+
 def load_from_string(source, component_name, filename=None):
-    tree = parser.parse(source)
-    analysis = analyser.analyse(tree)
-    generated_code = generator.generate(tree, analysis, component_name)
-    ast.fix_missing_locations(generated_code)
+    generated_code = _generate_code(source, component_name)
 
     pretty_code_print(generated_code)
 
-    # Compile the tree into a code object (module)
+    # Compile the AST into a code object (module)
     code = compile(generated_code, filename=filename or "<template>", mode="exec")
     # Execute the code as module and pass a dictionary that will capture
     # the global and local scope of the module
     module = {}
     exec(code, module)
     return module[component_name], module
+
+
+def _generate_code(source, component_name):
+    tree = parser.parse(source)
+    analysis = analyser.analyse(tree)
+    generated_code = generator.generate(tree, analysis, component_name)
+    ast.fix_missing_locations(generated_code)
+    return generated_code
 
 
 def pretty_code_print(tree):  # pragma: no cover
@@ -58,8 +81,6 @@ def pretty_code_print(tree):  # pragma: no cover
         result = plain_result
         return
 
-    syntax = Syntax(result, "python")
-    console.print(syntax)
-    # print("----")
-    # print(result)
-    # print("----")
+    # syntax = Syntax(result, "python")
+    # console.print(syntax)
+    print(result)
